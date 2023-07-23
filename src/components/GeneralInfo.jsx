97a21d93e8af0bba37aa810bb8b071b0ed9d011e
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-07-23 11:16:10 -0700

    initial commit

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,18 @@
+// name... email... phone number...
+// education...
+// past experience...
+import { useState } from 'react'
+
+function ContactInfo() {
+    
+    return (
+        <>
+            <div>name</div>
+            <div>email</div>
+            <div>phone</div>
+            <div>social media / linkedin</div>
+        </>
+    )
+}
+
+export default ContactInfo
