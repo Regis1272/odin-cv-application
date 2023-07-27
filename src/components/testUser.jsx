6715dce00706db903dc37ce4a1ba53e8d7d4e5b9
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-07-26 21:22:31 -0700

    nit

--- src/components/testUser.jsx
+++ src/components/testUser.jsx
@@ -42,22 +42,22 @@ const testFields = [
         ...blankFields,
         school: 'University of California, Los Angeles',
         titleOfStudy: 'Bachelor of Science in Computer Science',
-        dateOfStudy: '2018/06/01',
+        dateOfStudy: '2018-06-01',
     },
     {
         ...blankFields,
         company: 'Google',
         position: 'Software Engineer Intern',
-        dateEmployed: '2020/06/01',
-        dateLeft: '2020/09/01',
+        dateEmployed: '2020-06-01',
+        dateLeft: '2020-09-01',
         description: "During my internship on the Google Search team, I was exposed to a challenging and rewarding environment where I contributed to the development and improvement of the world's most widely used search engine. I collaborated with a talented team of engineers on enhancing search algorithms, refining query understandings, and ensuring user-centric design. The exposure to Google's culture of innovation and continuous learning has been instrumental in shaping my professional development."
     },
     {
         ...blankFields,
         company: 'Facebook',
         position: 'Software Engineer Intern',
-        dateEmployed: '2020/10/10',
-        dateLeft: '2021/02/01',
+        dateEmployed: '2020-10-10',
+        dateLeft: '2021-02-01',
         //  a long description
         description: 'As an Intern at Facebook, I had the opportunity to immerse myself in a fast-paced, innovative tech environment. My role primarily involved software development for enhancing user experiences and crafting scalable solutions for Facebook applications. This provided me with hands-on experience in coding, debugging, and deploying software, while refining my problem-solving abilities.'
 
